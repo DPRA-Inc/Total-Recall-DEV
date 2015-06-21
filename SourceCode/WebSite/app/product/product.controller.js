6b0b96ee2d57c6569e98aaf4eb21e2da8bb9f214
Steve-Frost <Steve.Frost@DPRA.com>
2015-06-21 11:43:48 -0400

    Start of the Product Feature Branch!

---
+++ SourceCode/WebSite/app/product/product.controller.js
@@ -0,0 +1,11 @@
+﻿angular.module('TotalRecall').controller('productcontroller', ProductController)
+
+function ProductController($http, productservice) {
+    var vm = this;
+           
+    vm.GetMoreInformation = function() {
+      
+    }
+
+};
+
---
+++ SourceCode/WebSite/app/product/product.service.js
@@ -0,0 +1,40 @@
+﻿angular.module('TotalRecall').factory('productservice', ProductService);
+
+function ProductService($http) {
+
+    var service = {
+        GetMoreInformation: GetMoreInformation
+    };
+
+    return service;
+
+    ///////////////////
+
+    function GetMoreInformation(searchItem, callback) {
+
+        var serviceUrl = 'QuickHandler.ashx?Command=GetMoreInformation';
+
+        $http({
+            method: 'POST',
+            url: serviceUrl,
+            data: searchItem         
+        }).
+            success(function (data, status, headers, config) {
+                if (data == undefined || data == "null") return;
+
+                callback(data);
+            }).
+            error(function (data, status, headers, config) {
+                $log.warn(data, status, headers, config)
+            });                 
+    }
+
+        
+
+}
+
+
+
+
+
+
