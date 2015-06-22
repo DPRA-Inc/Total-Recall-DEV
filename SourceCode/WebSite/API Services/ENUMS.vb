260ce117dcbb55bba2309aeb09b6456b7aedca28
Steve-Frost <Steve.Frost@DPRA.com>
2015-06-22 15:38:43 -0400

    Missing Files in Build

---
+++ SourceCode/WebSite/API Services/ENUMS.vb
@@ -0,0 +1,12 @@
+﻿
+Namespace ENUMS
+
+    Public Enum ErrorTypes As Byte
+        [Error] = 0
+        General = 1
+        Fatal = 2
+        Validation = 3
+        Warning = 4
+    End Enum
+
+End Namespace
