b7338944d40af05933a7a5efa45e555ba86c7e64
kjdpra <ken.jones@dpra.com>
2015-06-23 19:06:55 -0400

    Closes #27

--- SourceCode/WebSite/app/landing/landing.controller.js
+++ SourceCode/WebSite/app/landing/landing.controller.js
@@ -8,7 +8,7 @@ function landingcontroller($location, landingservice) {
     if (!angular.isObject(GlobalsModule.ShoppingList)) GlobalsModule.ShoppingList = [];
 
     vm.shoppingList = GlobalsModule.ShoppingList;
-           
+
     if (vm.shoppingList.length == 0) LoadPageInfo();
 
     //********************************
@@ -36,82 +36,89 @@ function landingcontroller($location, landingservice) {
         if (angular.isString(vm.textValue) && vm.textValue.replace(disallowedChars, "").length > 0) {
 
             var value = vm.textValue.replace(disallowedChars, "");
-        var region = "TN";
+            var region = "TN";
 
-        // Make the new item to be added to our list.
-        var item = [];
+            // Make the new item to be added to our list.
+            var item = [];
 
-        item.Keyword = value; // Product name
-        item.Rank = 'success'; // How Bad is it, Color Code.
-        item.IsLoading = true; // Indicates we are waiting on Return From Service.
-        item.HasClassI = false; // Indicates there is some Class I Data to show.
-        item.HasClassII = false;
-        item.HasClassIII = false;
-        item.HasEvents = false;
-        item.ClassICount = 0; 
-        item.ClassIICount = 0;
-        item.ClassIIICount = 0;
-        item.EventCount = 0;
-        item.IsClean = true;
+            item.Keyword = value; // Product name
+            item.Rank = 'success'; // How Bad is it, Color Code.
+            item.IsLoading = true; // Indicates we are waiting on Return From Service.
+            item.HasClassI = false; // Indicates there is some Class I Data to show.
+            item.HasClassII = false;
+            item.HasClassIII = false;
+            item.HasEvents = false;
+            item.ClassICount = 0;
+            item.ClassIICount = 0;
+            item.ClassIIICount = 0;
+            item.EventCount = 0;
+            item.IsClean = true;
 
-        vm.shoppingList.push(item);
+            vm.shoppingList.push(item);
 
-        vm.textValue = "";
+            vm.textValue = "";
 
-        // Differnt Ranks.
-        //Rank: 'warning'
-        //Rank: 'success',
-        //Rank: 'info',
-        //Rank: 'danger',
+            // Differnt Ranks.
+            //Rank: 'warning'
+            //Rank: 'success',
+            //Rank: 'info',
+            //Rank: 'danger',
 
-        var searchStr = value + "|" + region;
+            var searchStr = value + "|" + region;
 
-        var data = landingservice.GetIssues(searchStr,
-            function (result) {
-                
-                // Search for the Keyword in our list.
-                vm.shoppingList.forEach(function (product) {
+            var data = landingservice.GetIssues(searchStr,
+                function (result) {
+
+                    // Search for the Keyword in our list.
+                    vm.shoppingList.forEach(function (product) {
 
                         if (product.Keyword === result.Keyword) {
 
-                        if (result.EventCount > 0) {
-                            product.HasEvents = true;
-                            product.EventCount = result.EventCount;
-                            product.IsClean = false;
-                            product.Rank = "success";
-                        }
-                            
-                        if (result.ClassIIICount > 0) {
-                            product.HasClassIII = true;
-                            product.ClassIIICount = result.ClassIIICount;
-                            product.IsClean = false;
-                            product.Rank = "info";
-                        }
+                            if (result.EventCount > 0) {
+                                product.HasEvents = true;
+                                product.EventCount = result.EventCount;
+                                product.IsClean = false;
+                                product.Rank = "success";
+                            }
 
-                        if (result.ClassIICount > 0) {
-                            product.HasClassII = true;
-                            product.ClassIICount = result.ClassIICount;
-                            product.IsClean = false;
-                            product.Rank = "warning";
-                        }
+                            if (result.ClassIIICount > 0) {
+                                product.HasClassIII = true;
+                                product.ClassIIICount = result.ClassIIICount;
+                                product.IsClean = false;
+                                product.Rank = "info";
+                            }
 
-                        if (result.ClassICount > 0) {
-                            product.HasClassI = true;
-                            product.ClassICount = result.ClassICount;
-                            product.IsClean = false;
-                            product.Rank = "danger";
-                        }
+                            if (result.ClassIICount > 0) {
+                                product.HasClassII = true;
+                                product.ClassIICount = result.ClassIICount;
+                                product.IsClean = false;
+                                product.Rank = "warning";
+                            }
 
-                        product.IsLoading = false;
-                    }                                     
-                });
-            }
-        );
+                            if (result.ClassICount > 0) {
+                                product.HasClassI = true;
+                                product.ClassICount = result.ClassICount;
+                                product.IsClean = false;
+                                product.Rank = "danger";
+                            }
+
+                            product.IsLoading = false;
+                        }
+                    });
+                }
+            );
+        }
     }
+
+    vm.RemoveFromList= function(cartItem) {
+
+        var itemIndex = vm.shoppingList.indexOf(cartItem);
+        vm.shoppingList.splice(itemIndex, 1);
+
     }
 
     vm.ViewProductDetails = function (product) {
-       
+
         // set our shopping list to use later.
         GlobalsModule.ShoppingList = vm.shoppingList;
 
