bf2e4366097d97317a8349afda43beca5837000a
thomaskroll <thomas.kroll@dpra.com>
2015-06-24 15:07:21 -0400

    missing file from lee

---
+++ SourceCode/Tests/ApiDalc.Tests/OpenFda.Tests.vb
@@ -0,0 +1,26 @@
+﻿#Region " Imports "
+
+Imports Newtonsoft.Json.Linq
+Imports NUnit.Framework
+
+#End Region
+
+<TestFixture()>
+Public Class OpenFda
+
+    <TestCase()>
+    Public Sub AddSearchFilter_DrugEvent_WithDrugName_Returns()
+
+        'Dim mockRestClient = New Mocks.MockRestClient("AddSearchFilter_DrugEvent_Augmentin.json")
+        Dim mockRestClient = New Mocks.MockRestClient()
+        Dim drugName As String = "Augmentin"
+        Dim service = New ApiDalc.OpenFda(mockRestClient)
+        Dim result = service.AddSearchFilter(Enumerations.OpenFdaApiEndPoints.DrugEvent, Enumerations.FdaFilterTypes.DrugEventDrugName, New List(Of String)({drugName}))
+        Debug.WriteLine(result)
+        '(patient.drug.openfda.substance_name:Augmentin+patient.drug.openfda.brand_name:Augmentin+patient.drug.openfda.generic_name:Augmentin+patient.drug.medicinalproduct:Augmentin)
+        'Assert.AreEqual(Nothing, result)
+        Assert.AreNotEqual(Nothing, result)
+
+    End Sub
+
+End Class
