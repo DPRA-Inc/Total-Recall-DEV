61ff2d62b46eb97510b97ef4ab6a77d04095c405
Steve-Frost <Steve.Frost@DPRA.com>
2015-06-24 12:10:16 -0400

    Removed ocLazyLoad, we are not using it for this project.

--- SourceCode/WebSite/js/app.js
+++ SourceCode/WebSite/js/app.js
@@ -1,7 +1,6 @@
 (function () {
     angular.module("TotalRecall", [
         "ui.router",                    // Routing
-        "oc.lazyLoad",                  // ocLazyLoad
         "ui.bootstrap",                 // Ui Bootstrap
         "ngStorage",                    // Local Storage
         "openlayers-directive"
