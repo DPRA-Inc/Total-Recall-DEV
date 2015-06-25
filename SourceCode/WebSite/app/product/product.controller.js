c4d44b49f74786b395e2242ee9619c4703b27c99
Steve-Frost <Steve.Frost@DPRA.com>
2015-06-25 09:11:33 -0400

    Added Graph to Products Page.  WebApi Stubbed

--- SourceCode/WebSite/app/product/product.controller.js
+++ SourceCode/WebSite/app/product/product.controller.js
@@ -5,6 +5,8 @@ function ProductController($scope, $localStorage, $http, $modal, productservice)
     var vm = this;
 
     var fontSizeClass = "";
+    vm.lineOptions = [];
+    vm.lineData = [];
 
     if (angular.isString($localStorage.fontSizeClass))
     {
@@ -54,6 +56,7 @@ function ProductController($scope, $localStorage, $http, $modal, productservice)
     vm.SearchResultItem = GlobalsModule.SearchResultItem;
 
     LoadPageInfo();
+    LoadChartInfo();
 
     //*******************************************
 
@@ -119,8 +122,52 @@ function ProductController($scope, $localStorage, $http, $modal, productservice)
             templateUrl: 'app/product/productFullDetails.modal.html',
             controller: 'productcontroller as vm'
         });
-
-
     };
+
+    function LoadChartInfo() {
+
+        vm.lineOptions = {
+            scaleShowGridLines: true,
+            scaleGridLineColor: "rgba(0,0,0,.05)",
+            scaleGridLineWidth: 1,
+            bezierCurve: true,
+            bezierCurveTension: 0.4,
+            pointDot: true,
+            pointDotRadius: 4,
+            pointDotStrokeWidth: 1,
+            pointHitDetectionRadius: 20,
+            datasetStroke: true,
+            datasetStrokeWidth: 2,
+            datasetFill: true
+        };
+
+
+        vm.lineData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Example dataset",
+                    fillColor: "rgba(220,220,220,0.5)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "Example dataset",
+                    fillColor: "rgba(26,179,148,0.5)",
+                    strokeColor: "rgba(26,179,148,0.7)",
+                    pointColor: "rgba(26,179,148,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(26,179,148,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+    }
 };
 
--- SourceCode/WebSite/js/config.js
+++ SourceCode/WebSite/js/config.js
@@ -1,5 +1,10 @@
-function config($stateProvider, $urlRouterProvider) {
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
     $urlRouterProvider.otherwise("/index/landing");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
 
     // Tags: ROUTING, ADD PAGE
     $stateProvider
@@ -16,7 +21,25 @@ function config($stateProvider, $urlRouterProvider) {
         .state("index.product", {
             url: "/product",
             templateUrl: "app/product/product.html",
-            data: { pageTitle: "Product" }
+            data: { pageTitle: "Product" },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'angles',
+                            files: ['js/plugins/chartJs/angles.js', 'js/plugins/chartJs/Chart.min.js']
+                        },
+                        {
+                            name: 'angular-peity',
+                            files: ['js/plugins/peity/jquery.peity.min.js', 'js/plugins/peity/angular-peity.js']
+                        },
+                        {
+                            name: 'ui.checkbox',
+                            files: ['js/bootstrap/angular-bootstrap-checkbox.js']
+                        }
+                    ]);
+                }
+            }
         })
         .state("index.about", {
             url: "/about",
