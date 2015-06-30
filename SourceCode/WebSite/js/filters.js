d5689e91db4d2cc58893d99262716f8508a42d01
kjdpra <ken.jones@dpra.com>
2015-06-30 13:58:51 -0400

    When highlighting multi word keyword phrases they will now be split and highlighted separately.

--- SourceCode/WebSite/js/filters.js
+++ SourceCode/WebSite/js/filters.js
@@ -1,10 +1,15 @@
-﻿angular.module("TotalRecall").filter("highlight", function($sce) {
+﻿/*
+ * Allows for highlighting a keyword(s) in a phrase.
+ */
+angular.module("TotalRecall").filter("highlight", function ($sce) {
     return function (text, phrase) {
-
         if (angular.isString(text) && angular.isString(phrase)) {
-            if (phrase) text = text.replace(new RegExp("(" + phrase + ")", "gi"), "<span class='highlight'>$1</span>");
-        }
+            var keywords = phrase.split(" ");
 
+            for (var index = 0; index < keywords.length; index++) {
+                text = text.replace(new RegExp("(" + keywords[index] + ")", "gi"), "<span class='highlight'>$1</span>");
+            }
+        }
         return $sce.trustAsHtml(text);
     };
 })
