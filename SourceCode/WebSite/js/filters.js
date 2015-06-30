f90c0e3cf7da07070582fc73051802d93f823eae
kjdpra <ken.jones@dpra.com>
2015-06-30 12:13:53 -0400

    Closes #102

---
+++ SourceCode/WebSite/js/filters.js
@@ -0,0 +1,10 @@
+﻿angular.module("TotalRecall").filter("highlight", function($sce) {
+    return function (text, phrase) {
+
+        if (angular.isString(text) && angular.isString(phrase)) {
+            if (phrase) text = text.replace(new RegExp("(" + phrase + ")", "gi"), "<span class='highlight'>$1</span>");
+        }
+
+        return $sce.trustAsHtml(text);
+    };
+})
